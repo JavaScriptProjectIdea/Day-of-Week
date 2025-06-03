f041e3e6fe00a8fc5e17b69925fb464ab93f1c5e
Abdelwaheb AMMAR <aammar@ige-xao.com>
2025-06-04 00:20:08 +0100

    update on day of week

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
-  
+  $("#checkBtn").on("click",function(){
+   let dateOfUser=$("#dateInput").val();
+   const d=new Date(dateOfUser);
+   let givenDate=d.getDate();
+
+   let givenMonth=d.getMonth()+1;
+ 
+   let givenYear=d.getFullYear().toString();
+   let k=givenYear.substring(2,4);
+   let j=givenYear.substring(0,2);
+
+   let x=Math.floor((givenDate+((13*(givenMonth+1))/5)+Number(k)+(Number(k)/4)+5-Number(j))%7);
+   const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
+
+$("#dayResult").text(daysOfWeek[x]);
+  })
 });
